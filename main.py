3745aced2951604a0df91d8cbf4eadc344e341dc
Maksym Prysiazhnyi <mprysiazhnyi@godaddy.com>
2022-03-01 13:14:07 +0200

    POC bot deployment

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
-import time
+from flask import Flask, request, Response
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages import VideoMessage
+from viberbot.api.messages.text_message import TextMessage
+import logging
+import os
+
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
+
+app = Flask(__name__)
+
+viber = Api(BotConfiguration(
+    name='FirstAidRobot',
+    avatar='http://site.com/avatar.jpg',
+    auth_token=os.environ["BOT_TOKEN"]
+))
 
 
-#our function for counting from 0-20
-def counting():
-    for i in range(0,21):
-        if i==20:
-            print("counting at: "+str(i))
-            print("Done counting...")
-        else:
-            print("counting at: "+str(i))
-            time.sleep(5) #sleep for 5 seconds
-            # update
+@app.route('/', methods=['POST'])
+def incoming():
+    logging.debug("received request. post data: {0}".format(request.get_data()))
+    # every viber message is signed, you can verify the signature using this method
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status=403)
 
-#our main function
-if __name__ == '__main__':
-    counting() #call our counting method
+    # this library supplies a simple way to receive a request object
+    viber_request = viber.parse_request(request.get_data())
+
+    if isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message
+        # lets echo back
+        viber.send_messages(viber_request.sender.id, [
+            message
+        ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text="thanks for subscribing!")
+        ])
+    elif isinstance(viber_request, ViberFailedRequest):
+        logging.warn("client failed receiving message. failure: {0}".format(viber_request))
+
+    return Response(status=200)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=443, debug=True)
